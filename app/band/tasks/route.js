a0f50761529d86efa23ee6af86624a2eb46152c5
akradin <alex.kradin@gmail.com>
2017-01-18 17:54:21 -0500

    Set up user facing errors for tasks
    Began providing user feedback for users when they create or update
    tasks. There is also error messages for when a user enters in data
    improperly

--- app/band/tasks/route.js
+++ app/band/tasks/route.js
@@ -7,8 +7,15 @@ export default Ember.Route.extend({
   actions: {
     createTask(newTask){
       let task = this.get('store').createRecord('task', newTask);
-      // console.log("band id is", this.get('band'));
-      task.save();
+      console.log(task);
+      return task.save()
+      .then(() => this.get('flashMessages').success('Task created, keep making working hard!'))
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('Oh No! Something is not quite right. Make sure you have filled out required fields with the right format');
+        task.rollbackAttributes();
+      });
+
     },
     editTask(task){
       this.transitionTo('tasks/edit', task);
--- app/tasks/edit/route.js
+++ app/tasks/edit/route.js
@@ -7,8 +7,14 @@ export default Ember.Route.extend({
   actions : {
     saveUpdate(task){
       console.log("task is", task);
-      task.save();
-      this.transitionTo('bands');
+      return task.save()
+      .then(() => this.get('flashMessages').success('Updated! We are still working out the kinks, so for now you have to click to get back to your band'))
+      .then(() => this.transitionTo('bands'))
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('Oh No! Something is not quite right. Make sure you have filled out required fields with the right format');
+        task.rollbackAttributes();
+      });
     }
   }
 
