7c66af252bef1379e7e30fddbfd23a0fee980815
akradin <alex.kradin@gmail.com>
2017-01-24 15:28:59 -0500

    Fix bug in expenses and add stylistic changes
    Found a bug where expenses were taking in a description instead of
    details. Fixed this throughout the project and tested that it worked.
    
    Also added stylistic changes to tasks and expenses so they look
    more in line with contacts. Also added changes to the update forms.

--- app/components/expense-list/component.js
+++ app/components/expense-list/component.js
@@ -1,17 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  Expense:{
+  expense:{
     name: null,
-    description: null,
+    details: null,
     cost: null
   },
   actions : {
     createExpense(){
-      let expense = this.get('Expense');
-      this.set('Expense', {});
+      let expense = this.get('expense');
       expense.band = this.get('band');
       this.sendAction('createExpense', expense);
+      this.set('expense.name', '');
+      this.set('expense.details', '');
+      this.set('expense.cost', '');
+
     },
     editExpense(expense){
       this.sendAction('editExpense', expense);
--- app/expense/model.js
+++ app/expense/model.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  description: DS.attr('string'),
+  details: DS.attr('string'),
   cost: DS.attr('number'),
   band: DS.belongsTo('band')
 });
